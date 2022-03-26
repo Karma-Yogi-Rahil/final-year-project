dba1730d96860636a4c6666d9bc0e949ede206bd
Rahil <rahilmehta18@gmail.com>
2022-03-26 12:11:10 +0530

    updated

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from PIL import Image
+
+st.subheader("Image")
+image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+
+
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+	# To See details
+file_details = {"filename":image_file.name, "filetype":image_file.type,
+                              "filesize":image_file.size}
+st.write(file_details)
+
+    # To View Uploaded Image
+st.image(load_image(image_file),width=250)
