48701cfd92e58b6806cb6f5e9137ed37cdd81e1a
Rahil <rahilmehta18@gmail.com>
2022-03-26 12:45:01 +0530

    updated

---
+++ imagecaption.py
@@ -0,0 +1,4 @@
+import streamlit as st
+def app():
+    st.title('Image cation')
+    st.write('Welcome to app1')
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import streamlit as st
 from PIL import Image
-
-st.subheader("Image")
-image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-
+import imagecaption
+import currencycalculator
 
 
-def load_image(image_file):
-	img = Image.open(image_file)
-	return img
+PAGES = {
+    "Image Cation": imagecaption,
+    "CURRENCY ": currencycalculator
+}
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
+
+# st.subheader("Image")
+# image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+
+
+
+# def load_image(image_file):
+# 	img = Image.open(image_file)
+# 	return img
 
 	# To See details
-file_details = {"filename":image_file.name, "filetype":image_file.type,
-                              "filesize":image_file.size}
-st.write(file_details)
+# file_details = {"filename":image_file.name, "filetype":image_file.type,
+#                               "filesize":image_file.size}
+# st.write(file_details)
 
-    # To View Uploaded Image
-st.image(load_image(image_file),width=250)
+#     # To View Uploaded Image
+# st.image(load_image(image_file),width=250)
